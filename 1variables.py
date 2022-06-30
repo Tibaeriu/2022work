9191d42f136ba70804cea9fafba459dbc433f68b
Tiberiu Stanciu <tiberiu.stanciu@gmail.com>
2022-06-30 21:01:08 +0000

    1

--- 1variables.py
+++ 1variables.py
@@ -8,3 +8,7 @@ print(a_unu,a1)
 x = str(3) #caz in care vorbim despre declarare x ca fiind de tip text
 y = int(3) #caz in care vorbim despre declarare y ca fiind numarul 3
 z = float(3) #caz in care vorbim despre z ca fiind 3.0 (cu zecimala)
+# cum aflam despre o valoare ce fel de tip este? >> prin type()
+print(type(x))
+print(type(y))
+print(type(z))
