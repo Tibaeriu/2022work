b8c2a044009cecc9d7ec245e73364a11fc600d54
Tiberiu Stanciu <tiberiu.stanciu@gmail.com>
2022-06-30 21:11:22 +0000

    1.9

--- 1variables.py
+++ 1variables.py
@@ -12,3 +12,25 @@ z = float(3) #caz in care vorbim despre z ca fiind 3.0 (cu zecimala)
 print(type(x))
 print(type(y))
 print(type(z))
+x = "John"
+#is the same with
+x = 'John'
+a = 4
+A = "Sally"
+# A nu va supra-scrie a
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+"""
+myVariableName = "Tibaeriu"
+MyVariableName = 'Andreea'
+#declar valori pentru mai multe variabile in mod rapid
+m, n, o = "Alex", "Marian", "Cornel"
+print (m,n,o)
+print (m)
+print (n)
+print (o)
+#declar 1 valoare pentru 3 variabile
+d = f = g = "Test"
